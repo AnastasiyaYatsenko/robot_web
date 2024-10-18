c317978adadea7cafee033921471465fadac011d
AnastasiyaYatsenko <62345631+AnastasiyaYatsenko@users.noreply.github.com>
2024-10-18 17:01:43 +0300

    permission to unhold

--- templates/form_script.js
+++ templates/form_script.js
@@ -12,3 +12,19 @@ function ajaxsubmit({form='', query=''} = {})
     data: data
   });
 }
+
+function ajaxsubmit_reload({form='', query=''} = {})
+{
+  var data = $('#'+form).serialize();
+  if (query) {
+    if (data) { data = data + '&'; }
+    data = data + query;
+    location.reload();
+  }
+//  alert(data);
+  $.ajax({
+    type: "POST",
+    url: "/",
+    data: data
+  });
+}
